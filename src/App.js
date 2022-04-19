b87160912b1bc3668baec19e5162193f6fc7e86a
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-19 10:04:33 +0530

    add route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.scss";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:imdbID" component={MovieDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
