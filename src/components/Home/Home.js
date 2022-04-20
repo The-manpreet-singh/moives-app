7d7fc9079b51838a4e14897de96e40276cab674b
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-20 08:29:09 +0530

    fetch data from api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/movieApiKey";
 
 const Home = () => {
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("Error :", err);
+        });
+      console.log("The response from api", response);
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <div>
       <div className=""></div>
