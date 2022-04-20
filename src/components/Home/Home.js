b84f9c5390a5cb16ba8184b9108462b063d5079c
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-20 09:12:41 +0530

    add redux toolkit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,20 +2,24 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { APIKey } from "../../common/apis/movieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
+  const movieText = "Harry";
+  const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log("Error :", err);
         });
-      console.log("The response from api", response);
+      //console.log("The response from api", response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
-  }, []);
+  }, [dispatch]);
 
   return (
     <div>
