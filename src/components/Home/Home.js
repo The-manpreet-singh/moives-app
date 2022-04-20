03662dcec0f48687dee0ad7d7548eb76f05fcc5c
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-20 08:06:39 +0530

    import moive list

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import MovieListing from "../MovieListing/MovieListing";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className=""></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
