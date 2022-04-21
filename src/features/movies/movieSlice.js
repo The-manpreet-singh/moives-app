dbafb187068020795498db3cf9230937e81f87d8
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-21 08:17:36 +0530

    apply thunk Middleware redux toolkit

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/movieApiKey";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "Harry";
+    const response = await movieApi.get(
+      `?apikey=${APIKey}&s=${movieText}&type=movie`
+    );
+    //console.log("The response from api", response);
+    return response.data;
+  }
+);
 
 const initialState = {
   movies: {},
@@ -10,6 +24,18 @@ const movieSlice = createSlice({
   reducers: {
     addMovies: (state, { payload }) => {
       state.movies = payload;
+    },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfully");
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Rejected");
     },
   },
 });
