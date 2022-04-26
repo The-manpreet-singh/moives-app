60f18076b568b42d164247ee5bf9eed07ffe4ed4
Manpreet Singh <manpreetsingh@Manpreets-MacBook-Air.local>
2022-04-26 09:18:24 +0530

    add Search filter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import user from '../../images/user.png';
-import './Header.scss'
+import user from "../../images/user.png";
+import "./Header.scss";
 
 const Header = () => {
+  const [term, setTerm] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(term);
+  };
+
   return (
     <div className="header">
-      <Link to="/">
-        <div className="logo"> Moive App</div>
-      </Link>
+      <div className="logo">
+        <Link to="/">Moive App</Link>
+      </div>
+
+      <div className="search-bar">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={term}
+            placeholder="Search Movies and Shows"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <button type="sumbit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
+
       <div className="user-image">
         <img src={user} alt="user" />
       </div>
